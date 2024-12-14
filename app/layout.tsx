f98a479f612c575cf8afca99dca2ac44320ce33b
Nurman <38133707+nurmangoku@users.noreply.github.com>
2024-12-14 20:30:25 +0700

    third commit

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "A and M Building and Renovations Pty Ltd",
+  description:
+    "Trusted partner for building and renovation projects. We create durable, stylish, and functional spaces tailored to your vision. Get a free consultation now!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/aam-logo.jpg" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Unique Designs, Built with Precision",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.jpg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Modern Innovations for Your Dream Spaces",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/b2.jpg",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Trusted by Hundreds of Clients",
+    description: "Turning Your Vision into Reality",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "/b3.jpg",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Aesthetic Buildings, Crafted with Expertise",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/b4.jpg",
+    spareImg: "",
+  },
+
+  {
+    id: 5,
+    title: "Premium Materials for Every Construction",
+    description: "Turning Your Vision into Reality",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.jpg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Your Vision, Our Expertise. Start Your Project with Us Today!",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "A Fusion of Art and Construction Technology",
+    des: "We combine creativity and advanced construction technology to deliver innovative designs that are as functional as they are visually stunning.",
+    img: "/lift installation.jpg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Expert Team with Certified Excellence",
+    des: "Our highly skilled team is backed by industry certifications and years of experience.",
+    img: "/sauna at home.jpeg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "The Perfect Blend of Style and Strength",
+    des: "Our projects are designed to seamlessly combine aesthetic appeal with robust durability.",
+    img: "/stair.jpg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Support from Design to Completion",
+    des: "From the initial concept to the finishing touches, we provide end-to-end support to ensure your project is executed flawlessly.",
+    img: "/bathroom.jpg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Project Leadership and Client-Centered Execution",
+    desc: "Led multiple residential and commercial renovation projects with a focus on quality, efficiency, and client satisfaction.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.jfif",
+  },
+  {
+    id: 2,
+    title: "Innovative Design Expertise",
+    desc: "Designed innovative building layouts that blend functionality with modern aesthetics for diverse client needs.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.webp",
+  },
+  {
+    id: 3,
+    title: "Construction Management and Quality Assurance",
+    desc: "Managed construction teams, ensuring on-time project delivery while maintaining strict safety and quality standards.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.webp",
+  },
+  {
+    id: 4,
+    title: "Strategic Planning and Cost Optimization",
+    desc: "Developed cost-effective strategies and provided expert guidance throughout planning, design, and construction phases.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.jfif",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
