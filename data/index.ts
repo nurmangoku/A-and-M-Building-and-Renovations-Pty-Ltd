070b92fd8090a4c85b8f329576220abb1eecb1d4
Nurman <38133707+nurmangoku@users.noreply.github.com>
2024-12-26 20:22:23 +0700

    Update index.ts
    Eleven

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,7 @@
 export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
-  { name: "Testimonials", link: "#testimonials" },
+  { name: "Service", link: "#testimonials" },
   { name: "Contact", link: "#contact" },
 ];
 
